79a8446a522c66722fa5e225b84abc62a39e1c24
koehdcks <chan3_3@naver.com>
2022-12-25 23:24:11 +0900

    ch04

---
+++ ch04/src/Ex4_33.java
@@ -0,0 +1,13 @@
+
+public class Ex4_33 {
+	public static void main(String[] args) {
+		Loop1 : for(int i=2;i<=9;i++) {
+			for(int j=1;j<=9;j++) {
+				if(j==5)
+					continue Loop1;
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
+	}
+}
