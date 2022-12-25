e46645cfe3dfa02dd0acf247de4120a7ec9286e6
koehdcks <chan3_3@naver.com>
2022-12-25 21:31:32 +0900

    ch04

---
+++ ch04/src/Ex4_13.java
@@ -0,0 +1,11 @@
+
+public class Ex4_13 {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=1;i<=10;i++) {
+			sum += i;
+			System.out.printf("1부터 %2d 가지의 합: %2d%n",i,sum);
+		}
+	}
+}
---
+++ ch04/src/Ex4_14.java
@@ -0,0 +1,7 @@
+
+public class Ex4_14 {
+	public static void main(String[] args) {
+		for(int i=1,j=10;i<=10;i++,j--)
+			System.out.printf("%d \t %d%n",i,j);
+	}
+}
