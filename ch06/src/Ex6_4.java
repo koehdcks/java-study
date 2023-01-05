092f893700d5e15d6d2a6b3f8ca2345a5290aadd
koehdcks <chan3_3@naver.com>
2023-01-05 16:05:15 +0900

    test

--- ch06/src/Ex6_4.java
+++ ch06/src/Ex6_4.java
@@ -6,6 +6,7 @@ public class Ex6_4 {
 		long result2 = mm.subtract(5L, 3L);
 		long result3 = mm.multiply(5L, 3L);
 		double result4 = mm.divide(5L, 3L);
+		mm.printGugudan(3); 
 		
 		System.out.println("add(5L, 3L) = " +result1);
 		System.out.println("subtract(5L, 3L) = " +result2);
@@ -15,6 +16,19 @@ public class Ex6_4 {
 }
 
 class MyMath{
+	void printGugudan(int dan) {
+		if(!(2<=dan && dan<=9)) {
+			return; // 입력받은 단(dan)이 2~9가 아니면, 메서드 종료하고 돌아가기 
+		}
+		for (int i=1;i<=9;i++) {
+			System.out.printf("%d * %d = %d%n",dan,i,dan*i);
+		}
+		//return; // 반환타입이 void면 return 생략가능
+	}
+	long max(long a, long b) {
+		if(a>b) return a; // 조건식이 참일때와 거짓일때 둘다 리턴문을 줘야한다.
+		else return b;
+	}
 	long add(long a, long b) {
 		long result = a + b;
 		return result;
